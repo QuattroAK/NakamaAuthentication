cca1a5e27b37e4633b74102bb171c515f801ed2c
TylerHook <>
2024-04-18 20:20:07 +0700

    Added string extansions class

---
+++ Assets/Scripts/Core/Extensions/StringExtensions.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool TryEnum<T>(this string str, out T result) where T : struct
+            => Enum.TryParse(str, true, out result);
+    }
+}
